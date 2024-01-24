fc523d7f86951551f0b0a96e43a86f028288555e
mervecoskunn <merve_coskun2013@hotmail.com>
2024-01-24 13:12:30 +0100

    fixed bugs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,16 @@
-function sendMail(contactForm) {
-    emailjs.send("service_nd4xsh7", "resume", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    // return false; //to block from loading a new page
-}
+// function sendMail(contactForm) {
+//     emailjs.send("service_nd4xsh7", "resume", {
+//         "from_name": contactForm.name.value,
+//         "from_email": contactForm.emailaddress.value,
+//         "message": contactForm.projectsummary.value
+//     })
+//     .then(
+//         function(response) {
+//             console.log("SUCCESS", response);
+//         },
+//         function(error) {
+//             console.log("FAILED", error);
+//         }
+//     );
+//      return false; //to block from loading a new page
+// }
