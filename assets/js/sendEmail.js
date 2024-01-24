4b191840343b1d2f1df8edba70fa5bb1e0f38fd8
mervecoskunn <merve_coskun2013@hotmail.com>
2024-01-24 12:10:03 +0100

    fixed bugs about sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "merve", {
+    emailjs.send("service_nd4xsh7", "resume", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
